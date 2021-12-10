d37b923645856791bfe696797db82af69e3740ce
AtharvaChoudhari <atharva5258choudhari@gmail.com>
2021-12-10 12:17:56 +0530

    Read and Write logic added to data copier

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+import pandas as pd
+
 def main():
-    print("Hello World from Atharva")
+    # The file name is hardcoded and assigned to fp.
+    fp = 'C:\\Users\\Atharva Choudhari\\Research\\data\\retail_db_json\\order_items\\part-r-00000-6b83977e-3f20-404b-9b5f-29376ab1419e'
+
+    df = pd.read_json(fp, lines=True)
+
+    # print(df.count())
+    # print(df.describe())
+    # print(df.columns)
+    # print(df.dtypes)
+    # print(df[['order_item_order_id', 'order_item_subtotal']])
+    # print(df['order_item_order_id'] == 2)
+    print(df[df['order_item_order_id'] == 2])
+
 
 if __name__ == "__main__":
     main()
